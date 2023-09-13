99432fb17a349bb62ff852afa02db07f3ec2b627
Rich <richsaunders@protonmail.com>
2023-09-13 09:35:20 +0100

    Delete 102/class-07-script.js

--- 102/class-07-script.js
+++
@@ -1,7 +0,0 @@
-var list = document.getElementsByClassName("headers");
-			function clicked() {
-				for (var i = 0; i < list.length; i++) {
-				document.getElementsByClassName("headers")[i].innerHTML = "Changed by Class Name";
-			}
-      document.getElementById("paragraph").innerHTML = "Changed by ID";
-			}
